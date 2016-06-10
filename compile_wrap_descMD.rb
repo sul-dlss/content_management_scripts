0ba48f2d914c3edd02b5c9a865b10c0f85794a5e
Erin Fahy <efahy@stanford.edu>
2016-06-10 11:00:12 -0700

    Cleans up initial rubocop violations via auto-correct

--- compile_wrap_descMD.rb
+++ compile_wrap_descMD.rb
@@ -20,7 +20,7 @@ require 'nokogiri'
 
 # to run the script, it needs two input parameters:
 # 1. the folder of files to process
-# 2. the filename for the new file 
+# 2. the filename for the new file
 
 # the new file is written to the folder of files to process by default, unless you specify another location (a relative path) from this pwd
 
@@ -30,48 +30,43 @@ require 'nokogiri'
 folder = ARGV[0]
 descMD = ARGV[1]
 
-time = Time.now.strftime("%Y-%m-%d %I:%M:%S%p")
+time = Time.now.strftime('%Y-%m-%d %I:%M:%S%p')
 
 # change working directory to folder for processing files
 Dir.chdir(folder) do
+  # creates a new file for the compiled records
+  newFile = File.new(descMD.to_s, 'w')
+  newFile.write("<xmlDocs xmlns=\"http://library.stanford.edu/xmlDocs\" datetime=\"#{time}\" sourceFile=\"#{descMD}\">")
 
-	# creates a new file for the compiled records
-	newFile = File.new("#{descMD}", "w")
-	newFile.write("<xmlDocs xmlns=\"http://library.stanford.edu/xmlDocs\" datetime=\"#{time}\" sourceFile=\"#{descMD}\">")
+  # read the filenames in directory into an array for looking up
+  filenames = Dir.entries('.') #=> ["bc849vk0855.xml", "bc994zz9154.xml"]
 
-	# read the filenames in directory into an array for looking up
-	filenames = Dir.entries(".")  #=> ["bc849vk0855.xml", "bc994zz9154.xml"]
+  # iterate through filenames
+  filenames.each do |file|
+    if file =~ /[a-z]{2}[0-9]{3}[a-z]{2}[0-9]{4}\.xml/
+      # get druid from filename
+      druid = file.gsub(/\.xml/, '')
 
-	# iterate through filenames 
-	filenames.each  do | file | 
+      # open druid-named files
+      doc = Nokogiri::XML(open(file.to_s))
+      record = doc.root
 
-		if (file =~ /[a-z]{2}[0-9]{3}[a-z]{2}[0-9]{4}\.xml/)
-			# get druid from filename
-			druid = file.gsub(/\.xml/,"")
+      # add xmlDoc wrapper
+      newFile.write("<xmlDoc id=\"descMetadata\" objectId=\"#{druid}\">" + record.to_xml + '</xmlDoc>')
 
-			# open druid-named files
-			doc = Nokogiri::XML(open("#{file}"))
-			record = doc.root
+    elsif file =~ /druid.[a-z]{2}[0-9]{3}[a-z]{2}[0-9]{4}/
+      # get druid from the filename
+      druid = file.gsub(/druid./, '')
 
-			# add xmlDoc wrapper
-			newFile.write("<xmlDoc id=\"descMetadata\" objectId=\"#{druid}\">" + record.to_xml + "</xmlDoc>")
+      # open druid-named files
+      doc = Nokogiri::XML(open(file.to_s))
+      record = doc.root
 
-		elsif (file =~ /druid.[a-z]{2}[0-9]{3}[a-z]{2}[0-9]{4}/)
-			# get druid from the filename
-			druid = file.gsub(/druid./,"") 
+      # add xmlDoc wrapper
+      newFile.write("<xmlDoc id=\"descMetadata\" objectId=\"#{druid}\">" + record.to_xml + '</xmlDoc>')
 
-			# open druid-named files
-			doc = Nokogiri::XML(open("#{file}"))
-			record = doc.root
+    end
+  end
 
-			# add xmlDoc wrapper
-			newFile.write("<xmlDoc id=\"descMetadata\" objectId=\"#{druid}\">" + record.to_xml + "</xmlDoc>")
-			
-		end
-
-	end
-
-	newFile.write('</xmlDocs>')
-
+  newFile.write('</xmlDocs>')
 end
-
