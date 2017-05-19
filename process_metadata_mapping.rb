f178b2b1a492f6aafed9b2db18da4b3f2a49e2d3
Arcadia Falcone <arcadia@stanford.edu>
2017-05-19 15:56:31 -0700

    Fix conditional data to not register empty string as content

--- process_metadata_mapping.rb
+++ process_metadata_mapping.rb
@@ -73,7 +73,7 @@ sheets_in.each_with_pagename do |name, sheet|
     end
     # Delete data from row hash when required dependency is not present
     @datarules.each do |target, rule|
-      if data_fields.index(rule) == nil || row[data_fields.index(rule)] == nil || row[data_fields.index(rule)] =~ /^\s+$/
+      if data_fields.index(rule) == nil || row[data_fields.index(rule)] == nil || row[data_fields.index(rule)] =~ /^\s*$/
         data_out[target] = nil
       end
     end
