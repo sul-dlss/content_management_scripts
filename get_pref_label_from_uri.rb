c0dc34d42b8d17401628b64670a06e1b053fa44a
Arcadia Falcone <arcadia@stanford.edu>
2017-09-12 13:36:21 -0700

    Expanded comments

--- get_pref_label_from_uri.rb
+++ get_pref_label_from_uri.rb
@@ -10,16 +10,23 @@
 infile = ARGV[0]
 outfile = File.open("#{ARGV[1]}", 'w')
 
+# Write headers to output file
 outfile.write(['Source term', 'Preferred label', 'URI'].join("\t") + "\n")
 
+# Process input file
 File.foreach("#{infile}") do |line|
   fields = line.strip.split("\t")
+  # bash to query URI plus .nt extension and find prefLabel
   uri_match = `curl -sS "#{fields[1]}.nt" | grep "prefLabel"`
+  # Extract label value from prefLabel match
   label_match = uri_match.match(/"([^"]*)"/)
+  # If no match (URI does not return results, unable to identify prefLabel),
+  # log to output file
   if label_match == nil
     outfile.write([fields[0], 'NOT FOUND', fields[1]].join("\t") + "\n")
     next
   end
+  # If match, write to output file
   label = label_match[1]
   outfile.write([fields[0], label, fields[1]].join("\t") + "\n")
 end
