83e6bdaa45d7ac6ab8fcfd03c53f62095efc2a35
Arcadia Falcone <arcadia@stanford.edu>
2018-10-26 10:36:27 -0700

    Streamline namespace test

--- reverse_modsulator/spec/lib/reverse_modsulator_spec.rb
+++ reverse_modsulator/spec/lib/reverse_modsulator_spec.rb
@@ -1,17 +1,13 @@
 
 RSpec.describe ReverseModsulator do
   describe "use_correct_namespace" do
-    describe "use_default_namespace" do
-      subject = ReverseModsulator.new(FIXTURES_DIR, File.join(FIXTURES_DIR, "test.txt"))
-      it "uses the default namespace" do
-        expect(subject.namespace).to eq("xmlns")
-      end
+    default = ReverseModsulator.new(FIXTURES_DIR, File.join(FIXTURES_DIR, "test.txt"))
+    it "uses the default namespace" do
+      expect(default.namespace).to eq("xmlns")
     end
-    describe "use_optional_namespace" do
-      subject = ReverseModsulator.new(FIXTURES_DIR, File.join(FIXTURES_DIR, "test.txt"), {:namespace => "mods"})
-      it "uses the provided namespace" do
-        expect(subject.namespace).to eq("mods")
-      end
+    optional = ReverseModsulator.new(FIXTURES_DIR, File.join(FIXTURES_DIR, "test.txt"), {:namespace => "mods"})
+    it "uses the provided namespace" do
+      expect(optional.namespace).to eq("mods")
     end
   end
 end
