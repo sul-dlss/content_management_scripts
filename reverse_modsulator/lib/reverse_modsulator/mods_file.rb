c1ee28dfdf7b83cff4c8b12b89e41a2db093316e
Arcadia Falcone <arcadia@stanford.edu>
2018-10-26 15:02:16 -0700

    Extract self value, child values and attributes + tests

--- reverse_modsulator/lib/reverse_modsulator/mods_file.rb
+++ reverse_modsulator/lib/reverse_modsulator/mods_file.rb
@@ -13,8 +13,19 @@ class MODSFile
     @column_hash = {}
 
     @basic_elements = [
-      'name'
+      'titleInfo',
+      'name',
+      'genre',
+      'recordInfo'
     ]
+
+    @wrapper_elements = [
+      'titleInfo',
+      'name',
+      'role',
+      'recordInfo'
+    ]
+
   end
 
   def process_mods_file
@@ -23,6 +34,11 @@ class MODSFile
       template_element_nodes = @template.xpath("//#{@ns}:mods/#{@ns}:#{element}")
       mods_element_nodes.each_with_index do |n, i|
         @column_hash.merge!(extract_attributes(n, template_element_nodes[i]))
+        if @wrapper_elements.include?(element)
+          @column_hash.merge!(extract_child_attributes_and_values(n, template_element_nodes[i]))
+        else
+          @column_hash.merge!(extract_self_value(n, template_element_nodes[i]))
+        end
       end
     end
     return @column_hash
@@ -37,6 +53,41 @@ class MODSFile
       attributes[header_code.slice(2..-3)] = attr_value
     end
     return attributes
+  end
+
+  def extract_child_attributes_and_values(mods_node, template_node)
+    child_attributes_and_values = {}
+    child_element_names = get_child_element_names(mods_node)
+    child_element_names.each do |name|
+      child_mods_nodes = mods_node.xpath("xmlns:#{name}")
+      child_template_nodes = template_node.xpath("xmlns:#{name}")
+      child_mods_nodes.each_with_index do |n, i|
+        child_attributes_and_values.merge!(extract_attributes(n, child_template_nodes[i]))
+        child_attributes_and_values.merge!(extract_self_value(n, child_template_nodes[i]))
+        # if @paired_code_text_elements.keys.include?(name)
+        #   child_attributes_and_values.merge!(extract_code_text_values_and_attributes(n, child_template_nodes[i]))
+        # elsif @wrapper_elements.include?(name)
+        #   child_attributes_and_values.merge!(extract_child_attributes_and_values(n, child_template_nodes[i]))
+        # end
+      end
+    end
+    return child_attributes_and_values
+  end
+
+  def extract_self_value(mods_node, template_node)
+    self_value = {}
+    return {} if mods_node == nil || template_node == nil || mods_node.name == 'text' || @wrapper_elements.include?(mods_node.name)
+    header_code = template_node.content.strip
+    if header_code.start_with?('[[')
+      self_value[header_code.slice(2..-3)] = mods_node.content
+    end
+    return self_value
+  end
+
+  def get_child_element_names(mods_node)
+    return [] if mods_node == nil
+    child_element_names = mods_node.children.map {|x| x.name}.uniq.reject {|y| y == 'text'}.compact
+    return child_element_names
   end
 
 end
--- reverse_modsulator/spec/lib/reverse_modsulator/mods_file_spec.rb
+++ reverse_modsulator/spec/lib/reverse_modsulator/mods_file_spec.rb
@@ -1,11 +1,21 @@
 RSpec.describe(MODSFile) do
   describe "process_mods_file" do
     mods_file = MODSFile.new(File.join(FIXTURES_DIR, "bg730rr6720.xml"), Nokogiri::XML(File.open(TEMPLATE_FILE)), "xmlns")
+    mods = mods_file.process_mods_file
     it "returns a hash" do
-      expect(mods_file.process_mods_file.is_a?(Hash)).to be
+      expect(mods.is_a?(Hash)).to be
     end
-    it "extracts attributes" do
-      expect(mods_file.process_mods_file).to include("na1:type" => "personal", "na1:usage" => "primary", "na1:authority" => "naf", "na1:valueURI" => "http://id.loc.gov/authorities/names/n79113135")
+    it "extracts self attributes" do
+      expect(mods).to include("na1:type" => "personal", "na1:usage" => "primary", "na1:authority" => "naf", "na1:valueURI" => "http://id.loc.gov/authorities/names/n79113135")
+    end
+    it "extracts self value" do
+      expect(mods['ge1:genre']).to eq("photographs")
+    end
+    it "extracts child attributes" do
+      expect(mods).to include("rc:contentSourceAuthority" => "marcorg")
+    end
+    it "extracts child values" do
+      expect(mods).to include("na1:namePart" => "Nowinski, Ira", "na2:namePart" => "Smith, Jane")
     end
   end
 end
