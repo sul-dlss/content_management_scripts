d6519f944301352d5b1985c513967fb95b405117
Arcadia Falcone <arcadia@stanford.edu>
2018-10-26 11:47:03 -0700

    Build out ReverseModsulator class for directory-level processing and output

--- reverse_modsulator/lib/reverse_modsulator.rb
+++ reverse_modsulator/lib/reverse_modsulator.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 
 class ReverseModsulator
 
-  attr_reader :dir, :filename, :template_xml, :namespace
+  attr_reader :dir, :filename, :template_xml, :namespace, :data
 
   # @param [String] dir                        Input directory containing MODS XML files.
   # @param [String] filename                   The filename for the output spreadsheet.
@@ -14,6 +14,7 @@ class ReverseModsulator
     @filename = filename
 
     @outfile = File.open(@filename, 'w')
+    @data = {}
 
     if options[:template_file]
       @template_xml = Nokogiri::XML(File.open(options[:template_file]))
@@ -28,5 +29,65 @@ class ReverseModsulator
     end
   end
 
+  # @param [String] dir         Input directory containing MODS XML files.
+  # @param [Hash]   data        Empty hash to hold data output.
+  # @param [File]   outfile     File object for output rows.
+  def process_directory(dir, data, outfile)
+    Dir.foreach(dir) do |f|
+      next unless f.match('.xml')
+      druid = get_druid_from_filename(f)
+      data[druid] = process_mods_file(f)
+      write_output(data, outfile)
+    end
+  end
+
+  # @param [String] mods_filename   Name of MODS input file.
+  def get_druid_from_filename(mods_filename)
+    druid = mods_filename.gsub('druid:','').gsub('.xml','')
+    return druid
+  end
+
+  # @param [Hash]   data        Processed data output.
+  # @param [File]   outfile     File object for output rows.
+  def write_output(data, outfile)
+    rows = data_to_rows(data)
+    rows.each {|row| outfile.write(row.join("\t") + "\n")}
+  end
+
+  # @param [Hash]   data        Processed data output.
+  def data_to_rows(data)
+    rows = []
+    headers = get_ordered_headers(data)
+    rows << headers
+    data.each do |druid, column_hash|
+      row_out = [druid]
+      headers.each do |header|
+        if column_hash.keys.include?(header)
+          row_out = column_hash[header]
+        else
+          row_out << ""
+        end
+      end
+      rows << row_out
+    end
+    return rows
+  end
+
+  ## TODO: real ordering following replayable spreadsheet
+  # @param [Hash]   data        Processed data output.
+  def get_ordered_headers(data)
+    headers = get_headers(data)
+    ordered_headers = ['druid', 'sourceId'] + headers
+    return ordered_headers
+  end
+
+  # @param [Hash]   data        Processed data output.
+  def get_headers(data)
+    headers = []
+    data.each do |druid, column_hash|
+      headers << column_hash.keys
+    end
+    headers.flatten!.uniq!
+  end
 
 end
