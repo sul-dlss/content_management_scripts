5afc6830132db76882b2e9239313e060fa2b6e72
Arcadia Falcone <arcadia@stanford.edu>
2018-10-26 12:39:12 -0700

    Fix MODS file and template references

--- reverse_modsulator/lib/reverse_modsulator/mods_file.rb
+++ reverse_modsulator/lib/reverse_modsulator/mods_file.rb
@@ -1,14 +1,13 @@
 require 'nokogiri'
+require 'reverse_modsulator'
 
 class MODSFile
 
-  attr_reader :filename, :template
+  attr_reader :mods
 
   # @option options [String] :template_file    The full path to the desired template file (a spreadsheet).
-  def initialize(filename, template)
-    @filename = filename
-    @mods = Nokogiri::XML(File.open(filename)
-    @template = template
+  def initialize(filename)
+    @mods = Nokogiri::XML(File.open(filename))
   end
 
   def process_mods_file(mods_filename)
