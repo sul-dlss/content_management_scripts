97b2c7e8947a0bfa8e7238302f96273366a1fdad
Arcadia Falcone <arcadia@stanford.edu>
2018-10-26 13:04:15 -0700

    Add basic test for MODSFile

--- reverse_modsulator/spec/lib/reverse_modsulator_spec.rb
+++ reverse_modsulator/spec/lib/reverse_modsulator_spec.rb
@@ -1,6 +1,6 @@
 
 RSpec.describe ReverseModsulator do
-  describe "use_correct_namespace" do
+  describe "parse_parameters_correctly" do
     default = ReverseModsulator.new(FIXTURES_DIR, File.join(FIXTURES_DIR, "test.txt"))
     it "uses the default namespace" do
       expect(default.namespace).to eq("xmlns")
@@ -8,6 +8,12 @@ RSpec.describe ReverseModsulator do
     optional = ReverseModsulator.new(FIXTURES_DIR, File.join(FIXTURES_DIR, "test.txt"), {:namespace => "mods"})
     it "uses the provided namespace" do
       expect(optional.namespace).to eq("mods")
+    end
+  end
+  describe "process_mods_file" do
+    mods_file = MODSFile.new(File.join(FIXTURES_DIR, "bg730rr6720.xml"))
+    it "returns default data" do
+      expect(mods_file.process_mods_file).to eq({"ti1:title" => "title placeholder"})
     end
   end
 end
