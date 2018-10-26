c286849f0af09765224801352057f4725285991c
Arcadia Falcone <arcadia@stanford.edu>
2018-10-26 09:15:10 -0700

    Add fixtures directory

--- reverse_modsulator/spec/spec_helper.rb
+++ reverse_modsulator/spec/spec_helper.rb
@@ -17,6 +17,8 @@
 require 'rspec'
 # require 'reverse_modsulator'
 
+FIXTURES_DIR = File.expand_path('fixtures', __dir__)
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
