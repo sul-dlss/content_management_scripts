391fa0a30a00ca566fe0d4dc1a8bc3eddf813b50
Arcadia Falcone <arcadia@stanford.edu>
2018-10-26 12:04:37 -0700

    Add skeleton MODSFile class for testing

---
+++ reverse_modsulator/lib/reverse_modsulator/mods_file.rb
@@ -0,0 +1,16 @@
+require 'nokogiri'
+
+Class MODSFile
+
+  attr_reader :filename, :template
+
+  # @option options [String] :template_file    The full path to the desired template file (a spreadsheet).
+  def initialize(filename, template)
+    @filename = filename
+    @mods = Nokogiri::XML(File.open(File.join(indir, f)))
+    @template = template
+  end
+
+  def process_mods_file(mods_filename)
+    return {'ti1:title' => 'title placeholder'}
+  end
